f6e5227fc3e3814508e78484e55107f30d2de448
mgboyd407 <mike@getcellar.co>
2024-02-02 01:45:47 +0000

    W4D2

--- Demo.rb
+++ Demo.rb
@@ -1,4 +1,34 @@
-puts 2 + 2
-puts 5 % 7
-puts 3 % 3
-puts 3 % 9
+class Human
+    def walk
+        puts 'I can walk!!'
+    end
+
+    def talk
+        puts 'I can talk!!'
+    end
+end
+
+class Robot
+    def fly
+        puts 'I can fly!!'
+    end
+end
+
+class SuperHuman < Human
+    def super_walk
+        puts 'I can walk very fast!!'
+    end
+end
+
+class MegaHuman < SuperHuman
+end
+
+
+
+
+mike2 = SuperHuman.new
+mike2.super_walk
+mike2.walk
+
+mike3 = MegaHuman.new
+mike3.walk
